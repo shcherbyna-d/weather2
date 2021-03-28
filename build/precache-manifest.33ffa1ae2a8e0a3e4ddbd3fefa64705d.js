08919fb2d5bd5abbd9b98f55a640bca29010bf11
dmysh <dmysh@smart-it.com>
2021-03-28 21:55:20 +0300

    Add new files

---
+++ build/precache-manifest.33ffa1ae2a8e0a3e4ddbd3fefa64705d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb8e7c124a14370b0e2dfd08c52a32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155cb7952e5ebe3360b3",
+    "url": "/static/css/main.9320da23.chunk.css"
+  },
+  {
+    "revision": "99a4308275bbee67c9a4",
+    "url": "/static/js/2.9d7407e1.chunk.js"
+  },
+  {
+    "revision": "155cb7952e5ebe3360b3",
+    "url": "/static/js/main.8959ef04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
